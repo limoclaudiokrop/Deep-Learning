a73e17cd7574b74a6423c22cf5d066d8fb7e9f08
limoclaudiokrop <limoclaudiokrop@gmail.com>
2023-12-15 17:29:09 +0300

    Initial commit
    Firt commint

---
+++ CNN.py
@@ -0,0 +1,59 @@
+# Setup plotting
+print("Plotting Setup")
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+
+
+import pandas as pd
+red_wine = pd.read_csv('./red-wine.csv')
+print("Succesfull read file")
+
+red_wine.describe()
+# Create training and validation splits
+df_train = red_wine.sample(frac=0.7, random_state=0)
+df_valid = red_wine.drop(df_train.index)
+
+# Split features and target
+X_train = df_train.drop('quality', axis=1)
+X_valid = df_valid.drop('quality', axis=1)
+y_train = df_train['quality']
+y_valid = df_valid['quality']
+
+from tensorflow import keras
+from tensorflow.keras import layers
+print("Building Model")
+model = keras.Sequential([
+    layers.Dense(1024, activation='relu', input_shape=[11]),
+    layers.Dropout(0.3),
+    layers.BatchNormalization(),
+    layers.Dense(1024, activation='relu'),
+    layers.Dropout(0.3),
+    layers.BatchNormalization(),
+    layers.Dense(1024, activation='relu'),
+    layers.Dropout(0.3),
+    layers.BatchNormalization(),
+    layers.Dense(1),
+])
+
+model.compile(
+    optimizer='adam',
+    loss='mae',
+)
+
+print("Training on data")
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=256,
+    epochs=100,
+    verbose=0,
+)
+print("Training Completed")
+# Show the learning curves
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot();
